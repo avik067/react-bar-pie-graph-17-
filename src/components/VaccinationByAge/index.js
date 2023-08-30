a229a77ad369826bda44f9a8a26b9aa9b3a6d508
avik067 <avikbiswas067@gmail.com>
2023-08-30 15:04:38 +0000

    one bar color is not comming

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,10 +1,35 @@
-import {BarChart, Bar, CartesianGrid, XAxis, YAxis} from 'recharts'
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
 import './index.css'
 
 const VaccinationByAge = ({children}) => {
   console.log()
 
-  return <div>hi</div>
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={children}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#fecba6" />
+          <Cell name="45-60" fill="#b3d23f" />
+          <Cell name="Above 60" fill="#a44c9e" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="vertical"
+          verticalAlign="middle"
+          align="right"
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  )
 }
 
 export default VaccinationByAge
--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,9 +1,35 @@
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
 import './index.css'
 
-const VaccinationByGender = props => {
+const VaccinationByGender = ({children}) => {
   console.log()
 
-  return <div>VaccinationByGender</div>
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={children}
+          startAngle={0}
+          endAngle={180}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill="#fecba6" />
+          <Cell name="Female" fill="#b3d23f" />
+          <Cell name="Others" fill="#a44c9e" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="vertical"
+          verticalAlign="middle"
+          align="right"
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  )
 }
 
 export default VaccinationByGender
